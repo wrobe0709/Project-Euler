a562785ee85041cc758555904513870e9d60b020
System Administrator <wrobe0709@gmail.com>
2015-08-23 21:03:28 -0700

    Project-Euler additions

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Project-Euler
 Project Euler scripts.  
-
-7.26.15: Currently Pi script and blackjack-palindrome-largesum script.
---
+++ largest_prime_factor.py
@@ -0,0 +1,6 @@
+primelist = []
+for num in range(1, 13195):
+	if num % num == 0:
+		print num
+	primelist.append(num)
+print primelist
