339effadd567b2f517e835e8902a7e2a6567165a
Will Robertson <wrobe0709@gmail.com>
2016-02-19 07:06:12 -0800

    Updated to use arithmetic sequences for hackerrank

--- multiples_3_5.py
+++ multiples_3_5.py
@@ -1,6 +1,16 @@
-def sum_multiples():
-	biglist = []
-	for num in range(1000):
-		if num % 3 == 0 or num % 5 == 0:
-			biglist.append(num)
-	return sum(biglist)
+'''
+Re-factored this solution to use arithmetic sequences.
+Now it will work for large numbers, rather than
+just numbers under 1,000.
+'''
+
+def sum_multiples(factor, in_num):
+    a_n = (in_num - 1)/factor
+    sum_3 = a_n * (a_n + 1) / 2 * factor
+    return sum_3
+    
+t = int(raw_input())
+while t > 0:
+    in_num = int(raw_input())
+    print sum_multiples(3, in_num) + sum_multiples(5, in_num) - sum_multiples(15, in_num)
+    t -= 1
