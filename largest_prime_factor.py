b0290b174b2cfab984d892d00a08324e1271639c
System Administrator <wrobe0709@gmail.com>
2016-02-21 08:48:19 -0800

    largest prime factor for Hackerrank

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -1,28 +1,24 @@
-from itertools import *
-from math import sqrt
+def largest_prime_factor(in_num):
+    i = 2
+    largest_prime = 2
+    # checks up to the square root of in_num
+    while i*i <= in_num:
+        #checks if i is a factor
+        while in_num % i == 0:
+            # if i is a factor make it the largest prime factor   
+            largest_prime = i
+            # as long as i is a factor, repeadlty divide in_num down by i       
+            in_num /= i             
+        # check the next number
+        i += 1
+    # if what in_num ends up as is greater than what largest_prime is
+    # then set in_num to be the largest prime
+    if in_num > largest_prime:
+        largest_prime = in_num
+    return largest_prime 
 
-def is_prime(number):
-	primelist = []
-	stop = int(sqrt(number))
-	for x in range(1, stop + 1):
-		if number % x == 0:
-			primelist.append(x)
-	if len(primelist) == 1:
-		if primelist[0] == 1:
-			return True
-
-def largest_prime_factor(given):
-	stop = int(sqrt(given))
-	factorlist = []
-	for x in islice(range(stop), 1, stop, 2):
-		if given % x == 0:
-			if is_prime(x) == True:
-				factorlist.append(x)
-	print max(factorlist)
-
-if __name__ == '__main__':
-	largest_prime_factor(600851475143)
-
-	
-		
-
+t = int(raw_input())
+while t > 0:
+    in_num = int(raw_input())
+    print largest_prime_factor(in_num)
+    t -= 1
